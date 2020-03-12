7ee63baddc3fe723523028fa1d40d49a24911bfd
RichardEsquivel <rlesquivel@gmail.com>
2020-03-12 17:38:18 -0500

    Space issues

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -49,3 +50,54 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# This will allow mapping from player input into the linkage of rooms that has been established
+
+direction_map = {
+    'n': 'n_to',
+    's': 's_to',
+    'e': 'e_to',
+    'w': 'w_to'
+}
+
+
+def main():
+    # set the initial state of the player outside
+    player_1 = Player("Richard", room["outside"])
+
+    while True:
+        print(f"You are currently in {player_1.current_room.name}")
+        print(f"Description: {player_1.current_room.description}")
+        print("""------------Choose Your Direction---------------
+North: n | South: s | East:  e | West:  w | Quit: q
+---------------------------------------------------
+---------------------------------------------------
+""".lower())
+
+
+# logic to determine prompt that will occur when there are no items in the room or ther are items
+if len(player_1.current_room.items) <= 0:
+       print("There are no items in this room")
+else:
+        print("Current Items in the room")
+for item in player_1.current_room.items:
+        print(item)
+        user_input = input("Make your move:")
+        print("\n\n\"*10")
+
+ # logic for game action
+if user_input in dir_map.keys():
+    try:
+
+        player_1.current_room = getattr(
+        player_1.current_room, dir_map[user_input])
+		    except AttributeError:
+                print("----------------------------------")
+                print("No room here!")
+                print("----------------------------------")
+                print("\n\n")
+        elif user_input == 'q':
+            break
+
+if __name__ == "__main__":
+    main()
--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+
+    def__init__(self, name, description, items[]):
+        self.name = name
+        self.description = description
+        self.item = items
